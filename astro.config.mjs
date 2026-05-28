97b7e72f26525fcc187be04bfeba1482c20f5505
jlalonsoredon <jlalonsoredon@gmail.com>
2026-05-28 10:48:44 +0200

    Update output configuration to server and enable prerendering for index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: vercel(),
   site: "https://astro-scifi-ui.netlify.app/",
   vite: {
